768973cc9dace226e4dec0f6ef3d5bf3d5dd000a
pabdutra <pedroabd@al.insper.edu.br>
2025-04-11 00:55:01 -0300

    Atualizações no projeto: ajustes finais e build WebGL

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -6,8 +6,15 @@ public class UIManager : MonoBehaviour
     public GameObject endGamePanel;
     public TextMeshProUGUI pontuacaoText;
 
+    void Start()
+    {
+        // Garante que o painel comece desativado ao iniciar o jogo
+        endGamePanel.SetActive(false);
+    }
+
     void FixedUpdate()
     {
+        // Ativa o painel SOMENTE quando o jogo acabar
         if (GameController.gameOver)
         {
             endGamePanel.SetActive(true);
